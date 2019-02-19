9eaa7bfca35eed2e62191e97194c893703db55d4
Shokry <shokry92@gmail.com>
2019-02-19 22:29:13 +0200

    Add styled-components and shave off some fluff

---
+++ src/components/AppWrapper.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const AppWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: palevioletred;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+    "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+    sans-serif;
+`;
+
+export default AppWrapper;
